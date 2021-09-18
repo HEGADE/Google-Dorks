00fda96d632e437b254d4b44a84ad95a16072554
HEGADE <subramanyashegade@gmail.com>
2021-09-18 14:24:03 +0530

    desing imp

---
+++ src/uicomponents/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../css/nav.css";
+
+export const Nav = () => {
+  return (
+    <>
+      <nav className="navigation_container">
+        <div className="nav_heading">
+          <h3>Google Dorks</h3>
+        </div>
+
+        <div className="github_icon">
+          <a href="https://github.com/HEGADE/Google-Dorks"  target="_blank">
+            <img src="icon.png" alt="icon"   />
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
