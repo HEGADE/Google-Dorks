66c135cd79bacb273aac620251ccf8672967affe
HEGADE <subramanyashegade@gmail.com>
2021-09-18 20:08:28 +0530

    added more dorks

--- src/helper/userFinder.js
+++ src/helper/userFinder.js
@@ -3,5 +3,5 @@ export const userFinder = (username, bio, extra) => {
     return alert("username must be four character long");
   if (bio.trim()?.length === 0) return alert("fill up the bio filed");
   if (extra.trim()?.length === 0) return alert("fill up the extra filed filed");
-  window.location = `https://google.com/search?q=site: instagram.com ${username}  allintext: ${bio}  intext: ${extra}`;
+  window.location = `https://google.com/search?q=site: instagram.com ${username} inurl: ${username} allintext: ${bio}  intext: ${extra}`;
 };
